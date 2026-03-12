7e2b234bbe23c6364867a85e3d2a0fba9468f3ed
Yashas Pg <yashaspg123@gmail.com>
2026-03-12 23:55:17 +0530

    fix(router): resolve double prefix routing issue for vendor api
    Signed-off-by: Yashas Pg <yashaspg123@gmail.com>

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
 
+from .auth import router as auth_router
 from .user import router as user_router
 from .vendor import router as vendor_router
 
 api_router = APIRouter()
 api_router.include_router(vendor_router, tags=["Vendors"])
 api_router.include_router(user_router, tags=["Users"])
+api_router.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 
 
 # Add redirects for API documentation
